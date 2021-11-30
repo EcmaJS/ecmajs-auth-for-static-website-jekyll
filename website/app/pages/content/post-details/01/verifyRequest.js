dc2b21913ec9ad9ae5474156ba8af684264c621a
Dmitriy E <dsanich@saifas.com>
2021-11-30 14:48:26 +0300

    Added jQuery

--- website/app/pages/content/post-details/01/verifyRequest.js
+++ website/app/pages/content/post-details/01/verifyRequest.js
@@ -1,23 +1,22 @@
-import $ from "jquery";
-
-function httpGet()
-{
-  $.ajax({
-    type: "POST",
-    dataType: 'text',
-    url: "https://app.snipcart.com/api/orders/6d853811-ef49-4838-ae11-c4763cd73745",
-    username: 'user',
-    password: 'pass',
-    crossDomain : true,
-    xhrFields: {
-      withCredentials: true
-    }
+$(document).ready(function() {
+  $('#auth-go-to').on('click', function(){
+    $.ajax({
+      type: "POST",
+      dataType: 'text',
+      url: "https://app.snipcart.com/api/orders/6d853811-ef49-4838-ae11-c4763cd73745",
+      username: 'user',
+      password: 'pass',
+      crossDomain : true,
+      xhrFields: {
+        withCredentials: true
+      }
+    })
+    .done(function( data ) {
+        console.log("done");
+    })
+    .fail( function(xhr, textStatus, errorThrown) {
+      alert(xhr.responseText);
+      alert(textStatus);
+    });
   })
-  .done(function( data ) {
-      console.log("done");
-  })
-  .fail( function(xhr, textStatus, errorThrown) {
-    alert(xhr.responseText);
-    alert(textStatus);
-  });
 }
