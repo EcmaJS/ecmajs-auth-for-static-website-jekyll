36e3327c4b277052378613428cb093e511d38301
Dmitriy E <dsanich@saifas.com>
2021-12-02 13:43:55 +0300

    fix

--- website/app/pages/content/post-details/01/verifyRequest.js
+++ website/app/pages/content/post-details/01/verifyRequest.js
@@ -1,20 +1,31 @@
 $(document).ready(function() {
-  $('#auth-go-to').on('click', function(){
-    $.ajax({
-      type: "GET",
-      data: {
-        API_KEY: "ZjgyNjkwMDAtNGM3MS00ODA5LTgyNjEtMDA4YzVjY2E3MGJmNjM3NzM1OTQ4Mzk2Nzg5Nzkx"
-      },
-      dataType: 'jsonp',
-      url: "https://app.snipcart.com/api/orders/6d853811-ef49-4838-ae11-c4763cd73745",
-      crossDomain : true
-    })
-    .done(function( data ) {
-      console.log("done");
-    })
-    .fail( function(xhr, textStatus, errorThrown) {
-      alert(xhr.responseText);
-      alert(textStatus);
-    });
+  const secret = "ZjgyNjkwMDAtNGM3MS00ODA5LTgyNjEtMDA4YzVjY2E3MGJmNjM3NzM1OTQ4Mzk2Nzg5Nzkx"
+
+  const request = await fetch('https://app.snipcart.com/api/orders', {
+      headers: {
+          'Authorization': `Basic ${btoa(secret)}`,
+          'Accept': 'application/json'
+      }
   })
+
+  const result = await request.json()
+  console.log(result);
+  // $('#auth-go-to').on('click', function(){
+  //   $.ajax({
+  //     type: "GET",
+  //     data: {
+  //       API_KEY: "ZjgyNjkwMDAtNGM3MS00ODA5LTgyNjEtMDA4YzVjY2E3MGJmNjM3NzM1OTQ4Mzk2Nzg5Nzkx"
+  //     },
+  //     dataType: 'jsonp',
+  //     url: "https://app.snipcart.com/api/orders/6d853811-ef49-4838-ae11-c4763cd73745",
+  //     crossDomain : true
+  //   })
+  //   .done(function( data ) {
+  //     console.log("done");
+  //   })
+  //   .fail( function(xhr, textStatus, errorThrown) {
+  //     alert(xhr.responseText);
+  //     alert(textStatus);
+  //   });
+  // })
 })
