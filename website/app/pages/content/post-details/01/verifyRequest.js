364c0f96ae7d109559f19585563ab2e3463c6cea
Dmitriy E <dsanich@saifas.com>
2021-12-02 14:59:40 +0300

    Update checkRequest method (testing)

--- website/app/pages/content/post-details/01/verifyRequest.js
+++ website/app/pages/content/post-details/01/verifyRequest.js
@@ -13,7 +13,32 @@ async function checkData(){
     })
     const result = await request.json();
     if (email === result.email && token === result.token) {
-      alert('Успешно!');
+      let str = '';
+      str = ' ---
+      '       layout: default
+      '       ---
+      '       Post 01
+      '
+      '       ### Post 01 content
+      '
+      '       Text will be here
+      '
+      '       Pdf will be here
+      '
+      '       Images will be here
+      '
+      '       <button class="snipcart-add-item"
+      '         data-item-id="1"
+      '         data-item-price="1"
+      '         data-item-file-guid="ff4eef2b-8c2b-4481-8886-fe2fd7df69e4"
+      '         data-item-description="First document"
+      '         data-item-image="./images/1.jpg"
+      '         data-item-name="Document">
+      '         Add to cart
+      '       </button>'
+      document.getElementById('page-01').innerHTML += str;
+      // alert('Успешно!');
+      window.location.href = 'https://dover.ecmajs.dev/website/app/pages/content/post-details/01/'
     }
     else {
       alert('Вы ввели неверные данные');
