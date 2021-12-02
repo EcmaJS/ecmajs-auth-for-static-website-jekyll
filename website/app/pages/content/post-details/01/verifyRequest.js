bb72c87ca2059b3e1cfc1957f1aa6b59a37d4eaa
Dmitriy E <dsanich@saifas.com>
2021-12-02 13:01:28 +0300

    fixed request

--- website/app/pages/content/post-details/01/verifyRequest.js
+++ website/app/pages/content/post-details/01/verifyRequest.js
@@ -2,17 +2,12 @@ $(document).ready(function() {
   $('#auth-go-to').on('click', function(){
     $.ajax({
       type: "POST",
-      dataType: 'text',
+      dataType: 'jsonp',
       url: "https://app.snipcart.com/api/orders/6d853811-ef49-4838-ae11-c4763cd73745",
-      username: 'user',
-      password: 'pass',
-      crossDomain : true,
-      xhrFields: {
-        withCredentials: true
-      }
+      crossDomain : true
     })
     .done(function( data ) {
-        console.log("done");
+      console.log("done");
     })
     .fail( function(xhr, textStatus, errorThrown) {
       alert(xhr.responseText);
