a70b68e75aa104153ac5537fc61cc8fc99f4a866
Dmitriy E <dsanich@saifas.com>
2021-12-02 15:05:02 +0300

    Fix (testing)

--- website/app/pages/content/post-details/01/verifyRequest.js
+++ website/app/pages/content/post-details/01/verifyRequest.js
@@ -1,7 +1,7 @@
 API_KEY = "ST_MzdjYzYwN2EtZTIyNy00MjZkLTk3MGMtYzY3ODg3NjBjNzg0NjM3NzM1OTYyNTY5NjM2MzY5"
 GET_ORDER_URL = "https://app.snipcart.com/api/orders/"
 
-async function checkData(){
+async function checkRequest(){
   const email = document.getElementById('emailAuth').value;
   const token = document.getElementById('tokenAuth').value;
   if (email && token) {
@@ -14,28 +14,7 @@ async function checkData(){
     const result = await request.json();
     if (email === result.email && token === result.token) {
       let str = '';
-      str = ' ---
-      '       layout: default
-      '       ---
-      '       Post 01
-      '
-      '       ### Post 01 content
-      '
-      '       Text will be here
-      '
-      '       Pdf will be here
-      '
-      '       Images will be here
-      '
-      '       <button class="snipcart-add-item"
-      '         data-item-id="1"
-      '         data-item-price="1"
-      '         data-item-file-guid="ff4eef2b-8c2b-4481-8886-fe2fd7df69e4"
-      '         data-item-description="First document"
-      '         data-item-image="./images/1.jpg"
-      '         data-item-name="Document">
-      '         Add to cart
-      '       </button>'
+      str = 'Post 01'
       document.getElementById('page-01').innerHTML += str;
       // alert('Успешно!');
       window.location.href = 'https://dover.ecmajs.dev/website/app/pages/content/post-details/01/'
